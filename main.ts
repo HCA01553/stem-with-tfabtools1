f83d2dbf4a774a8d3054a4acd1bc31ca2e7df923
HCA01553 <116924963+HCA01553@users.noreply.github.com>
2022-12-03 14:25:54 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,9 @@ basic.showString("Start")
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P2) == 1) {
         pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.showString("ON")
     } else {
         pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.showString("OFF")
     }
 })
