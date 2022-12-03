e5a5349eeb2028878346e3a9ab8853df70e218b2
HCA01553 <116924963+HCA01553@users.noreply.github.com>
2022-12-03 14:17:44 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
+basic.showString("Start")
 basic.forever(function () {
-    if (stem.humanDetection()) {
-        stem.turnON()
-    } else if (false) {
-        stem.turnOFF()
+    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
     }
 })
